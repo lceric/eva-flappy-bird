962f677b58aff327e9a0d240c5c68ad855bf8dfa
Eric Lee <lichao@kaike.la>
2021-05-24 11:23:37 +0800

    feat: add background

--- src/gameObjects/background.ts
+++ src/gameObjects/background.ts
@@ -1,23 +1,49 @@
-import { GameObject } from '@eva/eva.js';
+import { Game, GameObject } from '@eva/eva.js'
 import { Img } from '@eva/plugin-renderer-img'
-export default function createBackground() {
+import { TilingSprite } from '@eva/plugin-renderer-tiling-sprite'
+
+export default function createBackground(
+  sceneWidth: number,
+  sceneHeight: number,
+  game: Game
+) {
   const bg = new GameObject('bg', {
-    size: { width: 750, height: 1624 },
+    size: { width: sceneWidth, height: sceneHeight },
     origin: { x: 0.5, y: 1 },
     position: {
       x: 0,
-      y: 120,
+      y: 0,
     },
     anchor: {
       x: 0.5,
       y: 1,
     },
-  });
+  })
 
   bg.addComponent(
     new Img({
       resource: 'bg',
     })
-  );
-  return bg;
+  )
+
+  const ground = new GameObject('sprite', {
+    size: { width: sceneWidth, height: 300 },
+    position: { x: 0, y: sceneHeight - 300 },
+  })
+
+  const groundTilingSprite = new TilingSprite({
+    resource: 'ground',
+    tileScale: { x: 1, y: 1.1 },
+    tilePosition: { x: 0, y: 0 },
+  })
+
+  ground.addComponent(groundTilingSprite)
+
+  bg.addChild(ground)
+
+  game.ticker.add(() => {
+    groundTilingSprite.tilePosition.x -= 1
+  })
+
+  return bg
 }
--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,96 @@
-import createBackground from './gameObjects/background';
-import createBasketFront from './gameObjects/board/basketFront';
-import createBoard from './gameObjects/board/board';
-import createBall from './gameObjects/ball';
-import createBtn from './gameObjects/btn';
-import resources from './resources';
+import createBackground from './gameObjects/background'
+import createReadyPanel from './gameObjects/ready/penel'
+import createBird from './gameObjects/bird'
+// import createBasketFront from './gameObjects/board/basketFront'
+// import createBoard from './gameObjects/board/board'
+// import createBall from './gameObjects/ball'
+// import createBtn from './gameObjects/btn'
+import resources from './resources'
 
-import { Game, resource } from '@eva/eva.js';
-import { RendererSystem } from '@eva/plugin-renderer';
-import { ImgSystem } from '@eva/plugin-renderer-img';
-import { EventSystem } from '@eva/plugin-renderer-event';
-import { SpriteAnimationSystem } from '@eva/plugin-renderer-sprite-animation';
-import { RenderSystem } from '@eva/plugin-renderer-render';
-import { TransitionSystem } from '@eva/plugin-transition';
-import { GraphicsSystem } from '@eva/plugin-renderer-graphics';
-import { TextSystem } from '@eva/plugin-renderer-text';
+import { Game, resource } from '@eva/eva.js'
+import { RendererSystem } from '@eva/plugin-renderer'
+import { ImgSystem } from '@eva/plugin-renderer-img'
+import { EventSystem } from '@eva/plugin-renderer-event'
+import { SpriteAnimationSystem } from '@eva/plugin-renderer-sprite-animation'
+import { RenderSystem } from '@eva/plugin-renderer-render'
+import { TransitionSystem } from '@eva/plugin-transition'
+import { GraphicsSystem } from '@eva/plugin-renderer-graphics'
+import { TextSystem } from '@eva/plugin-renderer-text'
+import { SpriteSystem } from '@eva/plugin-renderer-sprite'
+import {
+  TilingSpriteSystem,
+} from '@eva/plugin-renderer-tiling-sprite'
 
-resource.addResource(resources);
+resource.addResource(resources)
+
+const canvasEl: HTMLElement = document.querySelector('#canvas')
+const canvasWidth = canvasEl.offsetWidth
+const canvasHeight = canvasEl.offsetHeight
+const sceneWidth = 750
+const sceneHeight = (canvasHeight / canvasWidth) * 750
 
 const game = new Game({
+  frameRate: 60,
+  // autoStart: false,
   systems: [
     new RendererSystem({
-      canvas: document.querySelector('#canvas'),
-      width: 750,
-      height: 1484,
-      antialias: true,
+      canvas: canvasEl,
+      width: sceneWidth,
+      height: sceneHeight,
+      transparent: true,
+      resoution: 0.5,
     }),
     new ImgSystem(),
     new TransitionSystem(),
+    new SpriteSystem(),
     new SpriteAnimationSystem(),
     new RenderSystem(),
     new EventSystem(),
     new GraphicsSystem(),
     new TextSystem(),
+    new TilingSpriteSystem(),
   ],
-});
+})
 
-game.scene.transform.size.width = 750;
-game.scene.transform.size.height = 1484;
+game.scene.transform.size.width = sceneWidth
+game.scene.transform.size.height = sceneHeight
 
 const pos = {
   x: 500,
   y: 1100,
-};
+}
 
-const ball = createBall(pos);
-const { basetFront, playAnim } = createBasketFront();
-const btn = createBtn({
-  text: '投球',
-  transform: {
-    position: {
-      x: 0,
-      y: -120,
-    },
-    origin: {
-      x: 0.5,
-      y: 0.5,
-    },
-    anchor: {
-      x: 0.5,
-      y: 1,
-    },
-  },
-  callback: () => {
-    alert('还没做呢～一起来完善吧')
-  },
-});
+const birdPosition = {
+  x: 100,
+  y: 660,
+}
 
-game.scene.addChild(createBackground());
-game.scene.addChild(createBoard());
-game.scene.addChild(ball);
-game.scene.addChild(basetFront);
-game.scene.addChild(btn);
+const readyPanel = createReadyPanel()
+const { bird } = createBird(birdPosition)
+// const ball = createBall(pos)
+// const { basetFront, playAnim } = createBasketFront()
+// const btn = createBtn({
+//   text: '投球',
+//   transform: {
+//     position: {
+//       x: 0,
+//       y: -120,
+//     },
+//     origin: {
+//       x: 0.5,
+//       y: 0.5,
+//     },
+//     anchor: {
+//       x: 0.5,
+//       y: 1,
+//     },
+//   },
+//   callback: () => {
+//     alert('还没做呢～一起来完善吧')
+//   },
+// })
+game.scene.addChild(createBackground(sceneWidth, sceneHeight, game))
+game.scene.addChild(bird)
+game.scene.addChild(readyPanel)
 
-window.playAnim = playAnim;
-window.game = game;
+window.game = game

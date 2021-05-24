a18dc0d666e23d294a7bcd22f040ff2b5ca3ddc8
Eric Lee <lichao@kaike.la>
2021-05-24 11:23:17 +0800

    feat: update resource

--- src/resources.ts
+++ src/resources.ts
@@ -1,84 +1,57 @@
-import { RESOURCE_TYPE } from '@eva/eva.js';
+import { RESOURCE_TYPE } from '@eva/eva.js'
 export default [
-  {
-    name: 'basketball',
-    type: RESOURCE_TYPE.IMAGE,
-    src: {
-      image: {
-        type: 'png',
-        url: './statics/TB1WF1R0Ez1gK0jSZLeXXb9kVXa-99-99.png',
-      },
-    },
-    preload: true,
-  },
-  {
-    name: 'backboard',
-    type: RESOURCE_TYPE.IMAGE,
-    src: {
-      image: {
-        type: 'png',
-        url:
-          './statics/TB1a11YoRFR4u4jSZFPXXanzFXa-109-263.png',
-      },
-    },
-    preload: true,
-  },
   {
     name: 'bg',
     type: RESOURCE_TYPE.IMAGE,
     src: {
       image: {
         type: 'png',
-        url:
-          './statics/TB15Upxqk9l0K4jSZFKXXXFjpXa-750-1624.jpg',
+        url: './statics/bg.png',
       },
     },
     preload: true,
   },
   {
-    name: 'basketBack',
+    name: 'ground',
     type: RESOURCE_TYPE.IMAGE,
     src: {
       image: {
         type: 'png',
-        url:
-          './statics/TB1Xerd0AY2gK0jSZFgXXc5OFXa-184-175.png',
+        url: './statics/ground.png',
       },
     },
     preload: true,
   },
   {
-    name: 'boardIdle',
-    type: RESOURCE_TYPE.SPRITE_ANIMATION,
+    name: 'ready',
+    type: RESOURCE_TYPE.SPRITE,
     src: {
       image: {
         type: 'png',
-        url:
-          './statics/TB1LYwonSR26e4jSZFEXXbwuXXa-920-875.png',
+        url: './statics/ready/ready.png',
       },
       json: {
         type: 'json',
-        url:
-          './statics/3246284841596d87b60749e88e0e26cd.json',
+        url: './statics/ready/ready.json',
       },
     },
     preload: true,
   },
   {
-    name: 'boardGoal',
+    name: 'bird',
     type: RESOURCE_TYPE.SPRITE_ANIMATION,
     src: {
       image: {
         type: 'png',
         url:
-          './statics/TB1ob_c0EY1gK0jSZFCXXcwqXXa-552-525.png',
+          './statics/bird/bird.png',
       },
       json: {
         type: 'json',
         url:
-          './statics/dfefdd86474cded44bdc226549ae6d81.json',
+          './statics/bird/bird.json',
       },
     },
     preload: true,
   },
-];
+]
